da88b19705e97cc3164a0c0ffd9b947536d688ce
Derekcowan <derek@derekcowan.com>
2024-06-05 14:34:30 +0700

    metadata update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,31 +7,14 @@ import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "TAXI La Gi | Dịch Vụ Taxi Riêng | Gọi 0962 325023",
+  title: {
+    default: "TAXI La Gi | Dịch Vụ Taxi Riêng | Gọi 0962 325023",
+    template: "%s | TAXI La Gi",
+  },
   description:
     "Đặt taxi riêng tại La Gi với tài xế giàu kinh nghiệm. Chúng tôi cung cấp xe 4-7 chỗ thoải mái cho du lịch, đưa đón sân bay, đám cưới và các chuyến đi dài. Gọi ngay 0962 325023!",
-
-  openGraph: {
-    type: "website",
-    title: "TAXI La Gi | Dịch Vụ Taxi Riêng | Gọi 0962 325023",
-    description:
-      "Đặt taxi riêng tại La Gi với tài xế giàu kinh nghiệm. Chúng tôi cung cấp xe 4-7 chỗ thoải mái cho du lịch, đưa đón sân bay, đám cưới và các chuyến đi dài. Gọi ngay 0962 325023!",
-    images: [
-      {
-        url: "/Taxi La Gi Thinh 800x600.png",
-        width: 1200,
-        height: 630,
-        alt: "TAXI La Gi",
-      },
-    ],
-    url: "https://taxilagi.com",
-  },
   twitter: {
     card: "summary_large_image",
-    title: "TAXI La Gi | Dịch Vụ Taxi Riêng | Gọi 0962 325023",
-    description:
-      "Đặt taxi riêng tại La Gi với tài xế giàu kinh nghiệm. Chúng tôi cung cấp xe 4-7 chỗ thoải mái cho du lịch, đưa đón sân bay, đám cưới và các chuyến đi dài. Gọi ngay 0962 325023!",
-    images: ["/Taxi La Gi Thinh (2).png"],
   },
 };
 
