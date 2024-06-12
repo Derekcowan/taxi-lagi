9944c6ae9a3702d8839324833d8686e1b3438a7c
Derekcowan <derek@derekcowan.com>
2024-06-12 14:01:33 +0700

    refactor: Optimize image loading and update font imports in layout.tsx

--- src/components/AreaCovered.tsx
+++ src/components/AreaCovered.tsx
@@ -22,7 +22,7 @@ const AreaCovered = () => {
             src="/TAXI La Gi, Binh Thuan map.png"
             width="800"
             className="mx-auto rounded-lg object-cover"
-            quality={60}
+            quality={40}
           />
         </div>
         <div className="mx-auto grid max-w-5xl items-center gap-6 py-12 lg:grid-cols-3 lg:gap-12">
--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,25 +1,38 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import Script from "next/script";
 
 const GoogleAnalytics = () => {
-  return (
-    <>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
-      />
+  const [isLoaded, setIsLoaded] = useState(false);
 
-      <Script id="" strategy="lazyOnload">
-        {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-              });
-          `}
-      </Script>
-    </>
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoaded(true);
+    }, 4000); // Delay in milliseconds
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    isLoaded && (
+      <>
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+        />
+
+        <Script id="" strategy="lazyOnload">
+          {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
+                page_path: window.location.pathname,
+                });
+            `}
+        </Script>
+      </>
+    )
   );
 };
 
